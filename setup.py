5647e2d545c7b0220992ff5314c9b56693b47f9d
Rodrigo Gregori <rodrigo.gregori@gmail.com>
2025-03-31 22:04:02 -0300

    build: :bookmark: bump release version to 1.0.0 and add install_requires

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='dojocommons',
-    version='0.1.0',
+    name="dojocommons",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=[
-        # Adicione dependências aqui, se necessário
+        "pydantic~=2.10.6",
+        "duckdb~=1.2.1",
+        "boto3~=1.37.18",
+        "pydantic-settings~=2.8.1",
     ],
-    description='Classes comuns para o projeto Dojo Management.',
-    author='Rodrigo Gregori',
-    license='MIT',
+    description="Classes comuns para o projeto Dojo Management.",
+    author="Rodrigo Gregori",
+    license="MIT",
 )
